90a831393b159e6c711f6ba17cf5e257b66db978
cachho <admin@ch-webdev.com>
2024-03-20 17:17:42 +0100

    feat: fetch specific sha

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ API_TOKEN=$1
 PROJECT_ID=$2
 TEAM_ID=$3
 CYPRESS_OPTIONS=$4
+GITHUB_SHA=$5
 
 QUERY="projectId=$PROJECT_ID"
 TIMEOUT=10
@@ -21,15 +22,29 @@ fetch_deployment() {
     TEMP_RESPONSE_FILE=$(mktemp)
     curl -s -X GET "https://api.vercel.com/v6/deployments?${QUERY}" -H "Authorization: Bearer ${API_TOKEN}" > "$TEMP_RESPONSE_FILE"
     
-    # Extract each field using jq and store in variables
-    DEPLOYMENT_URL=$(jq -r '.deployments[0].url' < "$TEMP_RESPONSE_FILE")
-    DEPLOYMENT_STATE=$(jq -r '.deployments[0].state' < "$TEMP_RESPONSE_FILE")
-    DEPLOYMENT_READYSTATE=$(jq -r '.deployments[0].readyState' < "$TEMP_RESPONSE_FILE")
+    # Find the index of the deployment that matches the given GITHUB_SHA or default to 0
+    if [ -z "$GITHUB_SHA" ]; then
+        INDEX=0
+        echo "=> Using most recent deployment"
+    else
+        INDEX=$(jq -r --arg GITHUB_SHA "$GITHUB_SHA" '.deployments | map(.meta.githubCommitSha) | index($GITHUB_SHA)' < "$TEMP_RESPONSE_FILE")
+        echo "=> Using deployment for commit $GITHUB_SHA"
+        # If no matching deployment is found, default to the first deployment
+        if [ "$INDEX" = "null" ]; then
+            INDEX=0
+            echo "=> Using most recent deployment (github SHA $GITHUB_SHA not found)"
+        fi
+    fi
+
+    # Extract each field using jq and the found index
+    DEPLOYMENT_URL=$(jq -r --argjson INDEX "$INDEX" '.deployments[$INDEX].url' < "$TEMP_RESPONSE_FILE")
+    DEPLOYMENT_STATE=$(jq -r --argjson INDEX "$INDEX" '.deployments[$INDEX].state' < "$TEMP_RESPONSE_FILE")
+    DEPLOYMENT_READYSTATE=$(jq -r --argjson INDEX "$INDEX" '.deployments[$INDEX].readyState' < "$TEMP_RESPONSE_FILE")
     
     # Cleanup: Remove the temporary file
     rm "$TEMP_RESPONSE_FILE"
     
-    echo "=> Current Deployment State: $DEPLOYMENT_STATE"
+        echo "=> Current Deployment State: $DEPLOYMENT_STATE"
     echo "=> Ready State: $DEPLOYMENT_READYSTATE"
 }
 
